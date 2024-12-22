4956ee1d09918f14e6d2f8b6feb8d3d05a6e73c3
Mira <r1n0ku@yandex.ru>
2024-12-22 01:43:29 +0100

    abobus

---
+++ components/Circle1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <svg width="1366" height="982" viewBox="0 0 1366 982" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_f_226_3)">
+        <circle cx="265.5" cy="1036.5" r="600.5" fill="#FFEABF" fill-opacity="0.12"/>
+      </g>
+      <defs>
+        <filter id="filter0_f_226_3" x="-1235" y="-563" width="3001" height="3001" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+          <feGaussianBlur stdDeviation="400" result="effect1_foregroundBlur_226_3"/>
+        </filter>
+      </defs>
+    </svg>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,15 @@
 
 <template>
   <div>
-    <h1 class="text-3xl text-primary">RFAD SE</h1>
+    <div class="h-screen w-full absolute z-0">
+      <img src="assets/images/bg-circle1.webp" class="h-full w-full absolute min-w-[75rem] top-0 left-[-10rem] opacity-80 no-drag-no-select" />
+      <img src="assets/images/bg-circle2.webp" class="h-full w-full absolute min-w-[75rem] top-0 left-0 opacity-80 no-drag-no-select" />
+    </div>
+    <div class="z-10">
+      <h1 class="text-3xl mx-10 text-primary">test</h1>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
 </style>
